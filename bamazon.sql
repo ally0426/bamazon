c1c05598281a41de4a6a068cd19bcbd34c642c2c
Youngmi Lee <abinding@gmail.com>
2018-05-04 16:43:38 -0500

    second commit

--- bamazon.sql
+++ bamazon.sql
@@ -3,22 +3,17 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-  id INT NOT NULL AUTO_INCREMENT,
-  flavor VARCHAR(45) NULL,
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(45),
+  department_name VARCHAR(45),
   price DECIMAL(10,2) NULL,
-  quantity INT NULL,
-  PRIMARY KEY (id)
+  stock_quantity INT NULL,
+  PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (flavor, price, quantity)
-VALUES ("vanilla", 2.50, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("wii", "entertainment", 199.99, 100),("xbox", "entertainment", 399.99, 100), ("playstation", "entertainment", 299.99, 100), 
+       ("tshirt", "fasion", 9.99, 100), ("jeans", "fashion", 49.99, 100), ("shoes", "fashion", 99.99, 100), 
+       ("laptop", "IT", 799.99, 100), ("Galaxyphone", "IT", 899.99, 100), ("iPhone", "IT", 799.99, 100), 
+       ("router", "IT", 199.99, 100);
 
-INSERT INTO products (flavor, price, quantity)
-VALUES ("chocolate", 3.10, 120);
-
-INSERT INTO products (flavor, price, quantity)
-VALUES ("strawberry", 3.25, 75);
-
--- ### Alternative way to insert more than one row
--- INSERT INTO products (flavor, price, quantity)
--- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,93 +1,197 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
 
-  user: "root",
-  password: "",
-    insecureAuth : true
+// var newQuantity;
+// var requestedProduct;
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    user: "root",
+    password: "",
+    // insecureAuth : true,
     database: "bamazon"
 });
 
 connection.connect(function (err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
     readProducts();
 });
 
 function readProducts() {
-  console.log("Selecting all products...\n");
-    connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
-    if (err) throw err;
-    // Log all results of the SELECT statement
-    console.log(res);
-        // buyProduct();
-  });
+    console.log("Selecting all products...\n");
+    connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(res);
+        // const productNumbers = res.length;
+        // const { stock_quantity } = res  
+        // console.log("after const------------->", res[0].stock_quantity);     
+        buyProduct();
+    });
 }
 
-// function buyProduct() {
-//     console.log("Buying a product...\n");
+function buyProduct() {
+    console.log("Buying a product...\n");
 
-//     inquirer.prompt([
-//         {
-//             name: "item",
-//             type: "input",
-//             message: "What is the item you would like to buy?"
-//         },
-//         {
-//             name: "units",
-//             type: "input",
-//             message: "How many units would you like to buy?",
-//             validate: function (value) {
-//                 if (isNaN(value) === false) {
-//                     return true;
-//                 }
-//                 return false;
-//             }
+    inquirer.prompt([
+        {
+            name: "id",
+            type: "input",
+            message: "What is the item id you would like to buy?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        },
+        {
+            name: "units",
+            type: "input",
+            message: "How many units would you like to buy?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        }
+    ]).then(function (answer) {
+        console.log("Answer: ", answer);
+
+        var userId = answer.id;
+        console.log("Chosen item id: ", userId);
+
+        var userUnits = answer.units;
+        console.log("Chosen quantity from stock: ", userUnits, "\n");
+
+        connection.query("SELECT * FROM products WHERE ?", [{ item_id: answer.id }], function (err, res) {
+            if (err) throw err;
+            //grab the item_id from the table that matches
+            //return the item_id
+            console.log("Res: ", res);
+            var current_quantity = res[0].stock_quantity;
+            console.log("Current quantity in stock: ", current_quantity);
+            var price = res[0].price;
+            var remaining_quantity = current_quantity - answer.units;
+            console.log("Remaining quantity in stock: ", remaining_quantity);
+
+            if (current_quantity > answer.units) {
+
+                console.log("Amount Remaining: " + remaining_quantity);
+                console.log("Total Cost: " + (answer.units * price) + "\n");
+
+                connection.query("UPDATE products SET stock_quantity=? WHERE item_id=?",
+                    [
+                        remaining_quantity, answer.id
+                    ],
+
+                    // connection.query("UPDATE products SET stock_quantity=? WHERE item_id?",
+                    // 	[remaining_quantity, answer.id],
+
+                    function (err, res) {
+                        console.table(res);
+                    });
+
+                connection.query("SELECT * FROM products", function (err, res) {
+
+                    console.log("Here is an updated inventory: ");
+                    console.log("------------------------------- \n");
+                    console.table(res);
+                });
+
+            } else {
+                console.log("Insufficient amounts, please try again!");
+            }
+
+            connection.end();
+
+        });
+    })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // console.log("Updating products...\n");
+// // console.log("Product Name/Stock quantity: ", answer.item, " ", answer.units);
+// // console.log("stock_quantity", typeof(stock_quantity));
+// console.log("item_id======================", response.id);
+
+
+
+// // console.log("stock_quantity", typeof(stock_quantity));
+
+// // newQuantity = parseInt(stock_quantity) - parseInt(response.units);
+// // console.log("newQuantity", newQuantity);
+// choiceId = parseInt(response.id);
+// requestedQuantity = parseInt(response.items);
+// inventory = parseInt(response.stock_quantity);
+// console.log("choiceId", typeof (choiceId));
+// checkStocks();
+// debugger;
+//     });
+// }
+
+
+// function checkStocks(choiceId, requestedQuantity, inventory) {
+
+//     for (var i = 0; i < inventory.length; i++) {
+//         if (inventory[i].item_id === choiceId) {
+//             console.log("ID*********************", inventory[i].item_id);
+//             return inventory[i]
 //         }
-//     ]).then(function (answer) {
-//         console.log("Updating products...\n");
-//         console.log("Product Name/Stock quantity: ", answer.item, " ", answer.units);
-
-//         let newQuantity = stock_quantity - answer.units;
-//         let requestedProduct = answer.item;
-
-//         checkStocks();
 //     }
 
-// function checkStocks() {
-//             if (newQuantity <= 0) {
-//                 console.log("Insufficient quantity!");
-//                 buyProduct();
-//             } else {
-//                 var query = connection.query(
-//                     "UPDATE products SET ? WHERE ?",
-//                     [
-//                         {
-//                             quantity: newQuantity
-//                         },
-//                         {
-//                             name: requestedProduct
-//                         }
-//                     ],
-//                     function (err, res) {
-//                         console.log(res.affectedRows + " products updated!\n");
-
-//                     }
-//                 );
-
-//                 // logs the actual query being run
-//                 console.log(query.sql);
-//             }
-//             showPrice();
-//         }
-
-// function showPrice() {
-//             let productPrice = answer.units * answer.price;
-//             console.log(productPrice);
-//             connection.end();
-//         }
+// }
 
 
+
+// //             if (newQuantity <= 0) {
+// //                 console.log("Insufficient quantity!");
+// //                 buyProduct();
+// //             } else {
+// //                 var query = connection.query(
+// //                     "UPDATE products SET ? WHERE ?",
+// //                     [
+// //                         {
+// //                             quantity: parseInt(newQuantity)
+// //                         },
+// //                         {
+// //                             name: requestedProduct
+// //                         }
+// //                     ],
+// //                     function (err, res) {
+// //                         console.log("products updated!\n");
+
+// //                     }
+// //                 );
+
+// //                 // logs the actual query being run
+// //                 console.log(query.sql);
+// //             }
+// //             // showPrice();
+// //         }
+
+// // function showPrice() {
+// //             let productPrice = answer.units * answer.price;
+// //             console.log(productPrice);
+// //             connection.end();
+// //         }
