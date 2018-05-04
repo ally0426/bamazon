1a3ed31722bd2a7d3f4f9b3c91237d144f82a870
Youngmi Lee <abinding@gmail.com>
2018-05-04 16:42:36 -0500

    edited js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,87 +1,93 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
 
-  // Your username
   user: "root",
-
-  // Your password
   password: "",
-  database: "ice_creamDB"
+    insecureAuth : true
+    database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  createProduct();
+    readProducts();
 });
-
-function createProduct() {
-  console.log("Inserting a new product...\n");
-  var query = connection.query(
-    "INSERT INTO products SET ?",
-    {
-      flavor: "Rocky Road",
-      price: 3.0,
-      quantity: 50
-    },
-    function(err, res) {
-      console.log(res.affectedRows + " product inserted!\n");
-      // Call updateProduct AFTER the INSERT completes
-      updateProduct();
-    }
-  );
-
-  // logs the actual query being run
-  console.log(query.sql);
-}
-
-function updateProduct() {
-  console.log("Updating all Rocky Road quantities...\n");
-  var query = connection.query(
-    "UPDATE products SET ? WHERE ?",
-    [
-      {
-        quantity: 100
-      },
-      {
-        flavor: "Rocky Road"
-      }
-    ],
-    function(err, res) {
-      console.log(res.affectedRows + " products updated!\n");
-      // Call deleteProduct AFTER the UPDATE completes
-      deleteProduct();
-    }
-  );
-
-  // logs the actual query being run
-  console.log(query.sql);
-}
-
-function deleteProduct() {
-  console.log("Deleting all strawberry icecream...\n");
-  connection.query(
-    "DELETE FROM products WHERE ?",
-    {
-      flavor: "strawberry"
-    },
-    function(err, res) {
-      console.log(res.affectedRows + " products deleted!\n");
-      // Call readProducts AFTER the DELETE completes
-      readProducts();
-    }
-  );
-}
 
 function readProducts() {
   console.log("Selecting all products...\n");
-  connection.query("SELECT * FROM products", function(err, res) {
+    connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
     if (err) throw err;
     // Log all results of the SELECT statement
     console.log(res);
-    connection.end();
+        // buyProduct();
   });
 }
+
+// function buyProduct() {
+//     console.log("Buying a product...\n");
+
+//     inquirer.prompt([
+//         {
+//             name: "item",
+//             type: "input",
+//             message: "What is the item you would like to buy?"
+//         },
+//         {
+//             name: "units",
+//             type: "input",
+//             message: "How many units would you like to buy?",
+//             validate: function (value) {
+//                 if (isNaN(value) === false) {
+//                     return true;
+//                 }
+//                 return false;
+//             }
+//         }
+//     ]).then(function (answer) {
+//         console.log("Updating products...\n");
+//         console.log("Product Name/Stock quantity: ", answer.item, " ", answer.units);
+
+//         let newQuantity = stock_quantity - answer.units;
+//         let requestedProduct = answer.item;
+
+//         checkStocks();
+//     }
+
+// function checkStocks() {
+//             if (newQuantity <= 0) {
+//                 console.log("Insufficient quantity!");
+//                 buyProduct();
+//             } else {
+//                 var query = connection.query(
+//                     "UPDATE products SET ? WHERE ?",
+//                     [
+//                         {
+//                             quantity: newQuantity
+//                         },
+//                         {
+//                             name: requestedProduct
+//                         }
+//                     ],
+//                     function (err, res) {
+//                         console.log(res.affectedRows + " products updated!\n");
+
+//                     }
+//                 );
+
+//                 // logs the actual query being run
+//                 console.log(query.sql);
+//             }
+//             showPrice();
+//         }
+
+// function showPrice() {
+//             let productPrice = answer.units * answer.price;
+//             console.log(productPrice);
+//             connection.end();
+//         }
+
+
