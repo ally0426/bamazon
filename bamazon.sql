8e335cf5c8f95e7572902af14b616cf281640f7b
Youngmi Lee <abinding@gmail.com>
2018-05-05 10:14:37 -0500

    add gitignore file

--- bamazon.sql
+++ bamazon.sql
@@ -12,7 +12,7 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("wii", "entertainment", 199.99, 100),("xbox", "entertainment", 399.99, 100), ("playstation", "entertainment", 299.99, 100), 
+VALUES ("wii", "entertainment", 199.99, 100),("xbox one", "entertainment", 399.99, 100), ("playstation", "entertainment", 299.99, 100), 
        ("tshirt", "fasion", 9.99, 100), ("jeans", "fashion", 49.99, 100), ("shoes", "fashion", 99.99, 100), 
        ("laptop", "IT", 799.99, 100), ("Galaxyphone", "IT", 899.99, 100), ("iPhone", "IT", 799.99, 100), 
        ("router", "IT", 199.99, 100);
